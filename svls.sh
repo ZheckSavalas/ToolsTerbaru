dbd1f7566dd37259913dca326e1ded213e622dee
Zheck Savalas <57382953+ZheckSavalas@users.noreply.github.com>
2019-11-08 10:41:36 +0700

    Add files via upload

---
+++ svls.sh
@@ -0,0 +1,337 @@
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 svls.py
+echo
+# [SUBSCRIBE] Zacky Saksakame
+figlet -f standard Tools Terbaru | lolcat
+echo
+echo  " ____________________-=[Private Tools]=-____________________" | lolcat
+echo  "|                                                           |" | lolcat
+echo  "|              Root®Autor      : Zheck|Savalas              |" | lolcat
+echo  "|              Youtube         : Zacky Saksakame            |" | lolcat
+echo  "|              Thanks to       : StussyXTeam                |" | lolcat
+echo  "|___________________________________________________________|" | lolcat
+date | lolcat
+echo
+echo $i"===================================================="
+echo $i"|"$me" @"$i" |"$me" WARNING!!! SEBELUM MENGGUNAKAN TOOLS INI" $i"|"$me" @"$i" |"
+echo $i"|"$me" @"$i" |"$me" '-'SEBAIKNYA TIDAK DI PERJUAL BELIKAN'-'" $i"|"$me" @"$i" |"
+echo $i"===================================================="
+echo $i"|"$me" 0"$i" |"$pu"---'-'---》!!!ANDA BERSUMPAH!!!《---'-'---"$i"|"$me" 0"$i" |"
+echo $i"===================================================="
+echo $i"|"$me" 1"$i" |"$pu" REPORT FACEBOOK"
+echo $i"|"$me" 2"$i" |"$pu" BRUTE FORCE FACEBOOK"
+echo $i"|"$me" 3"$i" |"$pu" BRUTE FORCE FB CRACKER"
+echo $i"|"$me" 4"$i" |"$pu" MULTI BRUTE FORCE FACEBOOK"
+echo $i"|"$me" 5"$i" |"$pu" PROFILEGUARD FACEBOOK"
+echo $i"|"$me" 6"$i" |"$pu" OSIF(INFORMATION FB)"$me"[JANGAN DISALAH GUNAKAN]"
+echo $i"|"$me" 7"$i" |"$pu" YAHOO CLONING FACEBOOK"
+echo $i"|"$me" 8"$i" |"$pu" BOT REACTION FACEBOOK"
+echo $i"────────────────────────────────────────────────────"
+echo $i"|"$me" 9"$i" |"$pu" SPAM CALL v.1"
+echo $i"|"$me"10"$i" |"$pu" SPAM CALL v.2"
+echo $i"|"$me"11"$i" |"$pu" LITESPAM"
+echo $i"|"$me"12"$i" |"$pu" GCOSPAM"
+echo $i"|"$me"13"$i" |"$pu" SPAM WHATSAPP v.1"
+echo $i"|"$me"14"$i" |"$pu" SPAM WHATSAPP v.2"
+echo $i"|"$me"15"$i" |"$pu" SPAM SMS BRUTAL NEW"
+echo $i"────────────────────────────────────────────────────"
+echo $i"|"$me"16"$i" |"$pu" YUK SHOLAT"
+echo $i"|"$me"17"$i" |"$pu" TERMUX VERSI TERBARU"
+echo $i"|"$me"18"$i" |"$pu" HOME-TERMUX"
+echo $i"|"$me"19"$i" |"$pu" TOMBOL SPECIAL"
+echo $i"|"$me"20"$i" |"$pu" YOUTUBE-DL"
+echo $i"────────────────────────────────────────────────────"
+echo $i"|"$me"21"$i" |"$pu" LACAK LOKASI"
+echo $i"|"$me"22"$i" |"$pu" HACK CCTV NEGARA"
+echo $i"===================================================="
+echo $i"|"$me"23"$i" |"$pu" EXIT"
+echo $i"===================================================="
+echo $me"┌==="$bi"["$i"ZHECK|SAVALAS"$bi"]"$me"======"$bi"["$i""Masukan Nomor""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+clear
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg insta update && pkg install upgrade
+pkg install php
+pkg install git
+git clone https://github.com/GebangKiidiw/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg insta update && pkg install upgrade
+pkg install php
+pkg install git
+git clone https://github.com/siputra12/prank
+cd prank
+php call.php
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt upgrade && apt update
+apt install git 
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TobzCyberTeam/SpamWa
+cd SpamWa
+ls
+php SpamWa.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg insta update && pkg install upgrade
+pkg install php
+pkg install git
+git clone https://github.com/siputra12/prank
+cd prank
+php wa.php
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/MrTamfanX/MrTamfanSpam
+cd MrTamfanSpam
+chmod +x *
+sh MrTamfanSpam.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg instal pip 
+pkg instal git 
+pkg instal python 
+git clone https://github.com/kumpul4semut/yuksholat.git 
+cd yuksholat 
+python yuksholat.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg instal wget -y 
+pkg instal python 
+wget https://raw.githack.com/kumpul4semut/newtermux/master/key.py 
+chmod +x key.py 
+python key.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+apt update && apt upgrade
+pkg install figlet
+pkg install lolcat
+pkg install python2
+pkg install bash
+pkg install git
+git clone https://github.com/Rusmana-ID/home-termux
+cd home-termux
+ls
+python2 run.py
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg install python
+pkg insta git
+git clone https://github.com/karjok/terkey
+cd terkey 
+python terkey.py
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 21 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 22 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $pil = 23 ]
+then
+clear
+figlet -f slant "SHUTDOWN"|lolcat
+
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"Facebook :"$i" Zheck Savalas"
+echo $ku"YouTube  :"$i" Zacky Saksakame"
+echo $ku"WhatsApp :"$i" +6289691465691"
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+echo $pur"SUBSCRIBE MY CHANNEL { Zacky Saksakame }"
+exit
+fi
